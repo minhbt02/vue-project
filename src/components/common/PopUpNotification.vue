79a01674a1b12001e63d016eb174f346db8a72b9
minhbt02 <minhbt@symper.vn>
2023-10-24 15:18:31 +0700

    modify PopUpNotification

--- src/components/common/PopUpNotification.vue
+++ src/components/common/PopUpNotification.vue
@@ -1,33 +1,48 @@
 <template>
-  <div class="notification">
-    <p class="message text-h4">{{ successMessage }}</p>
-  </div>
+  <v-sheet
+    class="notification px-3 py-2 d-flex align-center"
+    height="70"
+    width="300"
+    :color="color"
+    :elevation="5"
+  >
+    <p class="message text-h6">{{ message }}</p>
+  </v-sheet>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { computed } from "vue";
+import { defineComponent } from "vue";
 export default defineComponent({
   name: "PopUpNotification",
   props: {
-    loginSuccess: Boolean,
-    registerSuccess: Boolean,
+    type: String,
+    message: String,
   },
-  setup() {
-    const message = ref("");
-    return {
-      message,
-    };
-  },
-  computed: {
-    successMessage() {
-      if (this.loginSuccess) {
-        return "Login successful!";
-      } else if (this.registerSuccess) {
-        return "Registration successful!";
-      } else {
-        return "";
+  setup(props) {
+    const color = computed(() => {
+      switch (props.type) {
+        case "success":
+          return "green";
+        case "fail":
+          return "red";
+        case "warning":
+          return "yellow";
+        default:
+          console.log("Wrong notification type!");
+          return "";
       }
-    },
+    });
+    return { color };
   },
 });
 </script>
+
+<style lang="scss">
+.notification {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  border-left: 5px solid #008c25;
+}
+</style>
