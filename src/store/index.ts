a8a84efde91bcbe6c9dfb0193bd710a10393b361
minhbt02 <minhbt@symper.vn>
2023-10-24 15:21:13 +0700

    add more state an mutation to controll PopUpNotification

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,9 @@ export default createStore({
   state: {
     loggedIn: false,
     id: -1,
+    loginSuccess: false,
+    registerSuccess: false,
+    displayPopUp: false,
   },
   getters: {
     loggedIn(state) {
@@ -13,16 +16,37 @@ export default createStore({
     id(state) {
       return state.id;
     },
+    loginSuccess(state) {
+      return state.loginSuccess;
+    },
+    registerSuccess(state) {
+      return state.registerSuccess;
+    },
+    displayPopUp(state) {
+      return state.displayPopUp;
+    },
   },
   mutations: {
-    login(state) {
+    login(state, uid) {
       state.loggedIn = true;
+      state.loginSuccess = true;
+      state.displayPopUp = true;
+      setTimeout(() => {
+        state.loginSuccess = false;
+        state.displayPopUp = false;
+      }, 3000);
+      state.id = uid;
     },
     logout(state) {
       state.loggedIn = false;
     },
-    identify(state, uid) {
-      state.id = uid;
+    register(state) {
+      state.registerSuccess = true;
+      state.displayPopUp = true;
+      setTimeout(() => {
+        state.registerSuccess = false;
+        state.displayPopUp = false;
+      }, 3000);
     },
   },
   actions: {},
