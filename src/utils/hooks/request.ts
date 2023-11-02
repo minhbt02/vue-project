31e547b22460bdd919e599371e523a5c5ca5f084
minhbt02 <minhbt@symper.vn>
2023-11-02 13:28:29 +0700

    export base url

--- src/utils/hooks/request.ts
+++ src/utils/hooks/request.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://localhost:3000";
 
 export const request = axios.create({
   baseURL: BASE_URL,
